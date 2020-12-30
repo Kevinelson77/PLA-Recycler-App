1c9a44b0327f4c4e20b400d4e53db1b6afdbff37
Kevin Nelson <Kevinelson77@gmail.com>
2020-12-29 21:30:34 -0500

    Added basic structure of CoreBluetooth functionality. Needs work for actual usability

--- SeniorDesign/ContentView.swift
+++ SeniorDesign/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
     var body: some View {
@@ -122,5 +123,59 @@ struct LineGraphView: View {
             .stroke(Color.white)
             .frame(width: 350, height: 200)
             .border(Color.white)
+    }
+}
+
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    var scale: CBPeripheral?
+    
+    let serviceUUID = CBUUID(string: "780A")
+    let kitchenScaleCharacteristicUUID = CBUUID(string: "8AA2")
+    @IBOutlet weak var weighLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager()
+        centralManager.delegate = self
+    }
+    
+    //MARK: - Central manager delegate
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        centralManager.stopScan()
+        scale = peripheral
+        centralManager.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices([serviceUUID])
+    }
+    
+    //MARK: - Peripheral delegate
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let service = peripheral.services?.first(where: { $0.uuid == serviceUUID }) {
+            peripheral.discoverCharacteristics([kitchenScaleCharacteristicUUID], for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristic = service.characteristics?.first(where: { $0.uuid == kitchenScaleCharacteristicUUID}) {
+            peripheral.setNotifyValue(true, for: characteristic)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let data = characteristic.value {
+            let weight: Int = data.withUnsafeBytes{ $0.pointee } >> 8 & 0xFFFFFF
+            let weight = data.withUnsafeBytes { $0.load(as: Int.self) } >> 8 & 0xFFFFFF
+            weighLabel.text = String(weight) + " g"
+        }
     }
 }
